72efb5c9bb8c91d340fe693a0646e6594668e44f
fieldstyler <tdfields93@gmail.com>
2020-07-08 09:55:03 -0600

    Created add and check product(s) test.

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -26,6 +26,17 @@ class ShoppingCartTest < Minitest::Test
     assert_equal [], shopping_cart.products
   end
 
+  def test_can_add_products_to_cart_and_check_them
+    shopping_cart = ShoppingCart.new('King Soopers', '30items')
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    assert_equal [], shopping_cart.products
+    assert_equal [product1], shopping_cart.add_product(product1)
+    assert_equal [product1], shopping_cart.products
+    # require "pry"; binding.pry
+    assert_equal [product1, product2], shopping_cart.add_product(product2)
+    assert_equal [product1, product2], shopping_cart.products
+  end
 
 
 end
