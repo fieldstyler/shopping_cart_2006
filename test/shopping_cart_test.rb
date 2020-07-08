41875f9317105432d08f409046923cfa85874458
fieldstyler <tdfields93@gmail.com>
2020-07-08 09:32:09 -0600

    Created ShoppingCart test file and test_it_exists test runs.

---
+++ test/shopping_cart_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/product'
+require './lib/shopping_cart'
+
+class ShoppingCartTest < Minitest::Test
+
+  def test_it_exists
+    shopping_cart = ShoppingCart.new('King Soopers', '30items')
+    assert_instance_of ShoppingCart, shopping_cart
+  end
+
+
+
+end
