ff03c14719b7d1d6d061d51e359ddbde1b7121df
fieldstyler <tdfields93@gmail.com>
2020-07-08 09:31:33 -0600

    Created ShoppingCart class and attributes.

---
+++ lib/shopping_cart.rb
@@ -0,0 +1,9 @@
+class ShoppingCart
+
+  attr_reader :name, :capacity, :products
+  def initialize(store_name, cart_capacity)
+    @name = store_name
+    @capacity = cart_capacity
+    @products = []
+  end
+end
