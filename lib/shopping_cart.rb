ec277ed97ef2c4d3421265949b1e1d60284f51e4
fieldstyler <tdfields93@gmail.com>
2020-07-08 09:39:39 -0600

    Changed capacity to convert to integer.

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -3,7 +3,7 @@ class ShoppingCart
   attr_reader :name, :capacity, :products
   def initialize(store_name, cart_capacity)
     @name = store_name
-    @capacity = cart_capacity
+    @capacity = cart_capacity.to_i
     @products = []
   end
 end
